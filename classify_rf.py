1d663f0aa042a35c2876d485d0f846a0643876b7
huntjk <jkhunt@stanford.edu>
2019-12-05 18:57:27 -0800

    rf fix data

--- classify_rf.py
+++ classify_rf.py
@@ -4,11 +4,8 @@ import pandas as pd
 
 def main():
     BASE_DIR = os.path.dirname((os.path.dirname(__file__)))
-    DATA_DIR = os.path.join(BASE_DIR, 'inputs/understanding_cloud_organization')
-    CROP_DIR = os.path.join(BASE_DIR, 'inputs/crops')
-
-    TRAIN_PATH = os.path.join(DATA_DIR, 'train_images/')
-    TRAIN_CSV_PATH = os.path.join(DATA_DIR, 'train.csv')
+    CROPS_DIR = os.path.join(BASE_DIR, 'inputs/crops/train_crops')
+    TRAIN_CSV = os.path.join(BASE_DIR, 'inputs/crops/train_crops.csv')
 
 if __name__ == '__main__':
     main()
